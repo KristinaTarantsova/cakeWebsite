1edab90358e75cb19f21a9868e38ae1aaaf9a1e3
Кристина <kristina-tarantsova@mail.ru>
2024-10-30 16:04:32 +0300

    добавит....

---
+++ README.md
@@ -0,0 +1,4 @@
+# Личный проект «Тортики»
+
+* Студент: [Кристина Т](https://up.htmlacademy.ru/adaptive/31/user/2445839).
+* Сайт: [Тортик ](https://kristinatarantsova.github.io/cackNew/).
---
+++ source/scripts/form.js
@@ -0,0 +1,17 @@
+// const inputs = document.querySelectorAll('.input-file');
+// Array.prototype.forEach.call(inputs, (input) => {
+//   const label = input.nextElementSibling,
+//     labelVal = label.querySelector('.your-sample__dounload').innerText;
+//   input.addEventListener('change', function (e) {
+//     let countFiles = '';
+//     if (this.files && this.files.length >= 1) {
+//       countFiles = this.files.length;
+//     }
+
+//     if (countFiles) {
+//       label.querySelector('.your-sample__dounload').innerText = `Выбрано файлов: ${ countFiles}`;
+//     } else {
+//       label.querySelector('.your-sample__dounload').innerText = labelVal;
+//     }
+//   });
+// });
